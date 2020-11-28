bb93e7697687bd57456d6420c95b64fd6680a5e6
Christoph Sonntag <sonntagc@fim.uni-passau.de>
2020-11-28 18:10:53 +0100

    Enforce dark mode at night

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,62 @@
 
-document.addEventListener('DOMContentLoaded', function(event) {
+/**
+ * Slide element down.
+ * @param  {Node} elem Element to show and hide
+ */
+function slideDown(elem) {
+  elem.style.maxHeight = '1000px';
+  elem.style.opacity   = '1';
+}
 
-  /**
-   * Slide element down.
-   * @param  {Node} elem Element to show and hide
-   */
-  function slideDown(elem) {
-    elem.style.maxHeight = '1000px';
-    elem.style.opacity   = '1';
-  }
+/**
+ * Slide element up.
+ * @param  {Node} elem Element
+ */
+function slideUp(elem) {
+  elem.style.maxHeight = '0';
+  once( 1, function () {
+    elem.style.opacity = '0';
+  });
+}
 
-  /**
-   * Slide element up.
-   * @param  {Node} elem Element
-   */
-  function slideUp(elem) {
-    elem.style.maxHeight = '0';
-    once( 1, function () {
-      elem.style.opacity = '0';
-    });
-  }
-
-  /**
-   * Fired once.
-   * @param seconds
-   * @param callback
-   */
-  function once (seconds, callback) {
-    var counter = 0;
-    var time = window.setInterval( function () {
-      counter++;
-      if ( counter >= seconds ) {
-        callback();
-        window.clearInterval( time );
-      }
-    }, 900 );
-  }
-
-
-  /**
-   * Slide toggle for a given id.
-   * @param id
-   */
-  var state = 0;
-  function slideToggle (id) {
-    var elem = document.getElementById(id);
-    if (state === 0) {
-      state = 1;
-      slideDown(elem);
-    } else {
-      state = 0;
-      slideUp(elem);
+/**
+ * Fired once.
+ * @param seconds
+ * @param callback
+ */
+function once (seconds, callback) {
+  var counter = 0;
+  var time = window.setInterval( function () {
+    counter++;
+    if ( counter >= seconds ) {
+      callback();
+      window.clearInterval( time );
     }
+  }, 900 );
+}
+
+
+/**
+ * Slide toggle for a given id.
+ * @param id
+ */
+var state = 0;
+function slideToggle (id) {
+  var elem = document.getElementById(id);
+  if (state === 0) {
+    state = 1;
+    slideDown(elem);
+  } else {
+    state = 0;
+    slideUp(elem);
   }
+}
 
 
+document.addEventListener('DOMContentLoaded', function(event) {
   const htmlTag = document.querySelector('html');
-  /**
-   * Switches the data-theme attribute on the html tag.
-   * @param theme
-   */
-  function switchTheme(theme) {
-    htmlTag.dataset.theme = `${theme}`;
-  }
 
+  // Checks the OS settings for preferred color scheme.
   const matchMediaPrefDark = window.matchMedia('(prefers-color-scheme: dark)');
   function startListeningToOSTheme() {
     matchMediaPrefDark.addEventListener('change', onSystemThemeChange);
@@ -83,10 +76,28 @@ document.addEventListener('DOMContentLoaded', function(event) {
   }
   startListeningToOSTheme();
 
+  /**
+   * Switches the data-theme attribute on the html tag.
+   * @param theme
+   */
+  function switchTheme(theme) {
+    htmlTag.dataset.theme = `${theme}`;
+  }
+
+  today = new Date(); //outputs date in 24hrs format
+  if (today.getHours() > 7 && today.getHours() < 19) {
+    // its day, use preferred setting
+  } else {
+    //its night, enforce dark mode.
+    switchTheme('dark')
+  }
+
+  /**
+   * Toggles the current state of the dark/night mode.
+   */
   document.getElementById('dark-light-switch').addEventListener('click', function () {
     stopListeningToOSTheme();
     var theme = htmlTag.dataset.theme;
-    console.log(theme);
     if (theme === "light") {
       switchTheme("dark");
     } else {
